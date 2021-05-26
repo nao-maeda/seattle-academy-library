b308154854b03601603ce98f3b96ccf8d2957433
mmmmm <mnao@maedanaonoMacBook-Pro.local>
2021-05-26 12:36:20 +0900

    並び替え機能完了

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -31,11 +31,13 @@
         <a href="<%=request.getContextPath()%>/addBook" class="btn_add_book">書籍の追加</a> 
         <a href="<%=request.getContextPath()%>/bulkBook" class="btn_bulk_book">一括登録</a>
         <select id=item>
-            <option value="${bookInfo.title}">書籍名</option>
-            <option value="${bookInfo.author}">著者名</option>
+            <option value="0">書籍名</option>
+            <option value="1">著者名</option>
             <option value="2">出版社</option>
             <option value="3">出版日</option>
         </select>
+        <input id="ascbtn" class="sortbtn" type="radio" name="sortCheckBox" value="asc" checked><span>昇順</span> 
+        <input id="descbtn" class="sortbtn" type="radio" name="sortCheckBox" value="desc"><span>降順</span>
         
         <form id="form1" action="<%=request.getContextPath()%>/searchBook">
             <input id="checkbtn" type="radio" name="checkBox" value="perfectMatching" checked><span>完全一致</span>
@@ -49,6 +51,7 @@
             </c:if>
             <div>
                 <div class="booklist">
+                
                     <c:forEach var="bookInfo" items="${bookList}">
                         <div class="books">
                             <form method="post" class="book_thumnail" action="<%=request.getContextPath()%>/details">
--- src/main/webapp/resources/js/sort.js
+++ src/main/webapp/resources/js/sort.js
@@ -1,10 +1,270 @@
 /**
  * 
  */
-$('#item').change(function(){
+$(function() {
+	$('#item').change(function() {
+		//セレクタを選択した時
 	
-	var r = $('option:selected').val();
-    
-    console.log(r);
+		var result = $('option:selected').val();
+		var array = [];
+		$(".books").each(function(i) {//全ての項目データを取得
+			array[i] = {
+				title: $(this).find(".book_title").text(),
+				author: $(this).find(".book_aouthor").text(),
+				publisher: $(this).find(".book_publisher").text(),
+				publishDate: $(this).find(".book_publishDate").text(),
+				source: $(this).html()
+			};
+		});
+
+		if  (result == 0){
+			//書籍名をセレクト
+			if ($("#ascbtn:checked").prop("checked")) {
+				//昇順が選択されているとき
+				//昇順に並び替え
+				array.sort(function(a, b) {
+					if (a.title > b.title) {
+						return 1;
+					} else if (a.title < b.title) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if ($("#descbtn:checked").prop("checked")) {
+				//降順が選択されているとき
+				//降順に並び替え
+				array.sort(function(a, b) {
+					if (a.title < b.title) {
+						return 1;
+					} else if (a.title > b.title) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			}
+		}
+
+		if (result == 1) {
+			//著者名をセレクト
+			if ($("#ascbtn:checked").prop("checked")) {
+				//昇順に並び替え
+				array.sort(function(a, b) {
+					if (a.author > b.author) {
+						return 1;
+					} else if (a.author < b.author) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			} else if ($("#descbtn:checked").prop("checked")) {
+				//降順に並び替え
+				array.sort(function(a, b) {
+					if (a.author < b.author) {
+						return 1;
+					} else if (a.author > b.author) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			}
+		}
+
+		if (result == 2) {
+			//出版社をセレクト
+			if ($("#ascbtn:checked").prop("checked")) {
+				//昇順に並び替え
+				array.sort(function(a, b) {
+					if (a.publisher > b.publisher) {
+						return 1;
+					} else if (a.publisher < b.publisher) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			} else if ($("#descbtn:checked").prop("checked")) {
+				//降順に並び替え
+				array.sort(function(a, b) {
+					if (a.publisher < b.publisher) {
+						return 1;
+					} else if (a.publisher > b.publisher) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			}
+		}
+
+		if (result == 3) {
+			//出版日をセレクト
+			if ($("#ascbtn:checked").prop("checked")) {
+				//昇順に並び替え
+				array.sort(function(a, b) {
+					if (a.publishDate > b.publishDate) {
+						return 1;
+					} else if (a.publishDate < b.publishDate) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			} else if ($("#descbtn:checked").prop("checked")) {
+				//降順に並び替え
+				array.sort(function(a, b) {
+					if (a.publishDate < b.publishDate) {
+						return 1;
+					} else if (a.publishDate > b.publishDate) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+			}
+		}
+		for (var j = 0; j < array.length; j++) {
+			$(".books").eq(j).html(array[j].source);
+		}
+	});
 	
-})
+	
+	$('input:radio[name="sortCheckBox"]').change(function() {
+		//昇順降順のradioboxを選択した時
+		
+		var result = $('option:selected').val();
+		const str1 = $('input:radio[name="sortCheckBox"]:checked').val();
+		var array = [];
+		$(".books").each(function(i) {
+			array[i] = {
+				title: $(this).find(".book_title").text(),
+				author: $(this).find(".book_author").text(),
+				publisher: $(this).find(".book_publisher").text(),
+				publishDate: $(this).find(".book_publishDate").text(),
+				source: $(this).html()
+			};
+		});
+
+		if  (str1 == 'asc'){
+			//昇順にチェックを入れた時
+			if (result == 0) {
+				//書籍名が選択されている場合
+				//書籍名の昇順を表示
+				array.sort(function(a, b) {
+					if (a.title > b.title) {
+						return 1;
+					} else if (a.title < b.title) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 1) {
+				//著者名が選択されている場合
+				//著者名の昇順を表示
+				array.sort(function(a, b) {
+					if (a.author > b.author) {
+						return 1;
+					} else if (a.author < b.author) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 2) {
+				//出版社が選択されている場合
+				//出版社の昇順を表示
+				array.sort(function(a, b) {
+					if (a.publisher > b.publisher) {
+						return 1;
+					} else if (a.publisher < b.publisher) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 3) {
+				//出版日が選択されている場合
+				//出版日の昇順を表示
+				array.sort(function(a, b) {
+					if (a.publishDate > b.publishDate) {
+						return 1;
+					} else if (a.publishDate < b.publishDate) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			}
+		}
+		
+		if (str1 == 'desc'){
+			//降順にチェックを入れた時
+			if (result == 0) {
+				//書籍名が選択されている場合
+				//書籍名の降順を表示
+				array.sort(function(a, b) {
+					if (a.title < b.title) {
+						return 1;
+					} else if (a.title > b.title) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 1) {
+				//著者名が選択されている場合
+				//著者名の降順を表示
+				array.sort(function(a, b) {
+					if (a.author < b.author) {
+						return 1;
+					} else if (a.author > b.author) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 2) {
+				//出版社が選択されている場合
+				//出版社の降順を表示
+				array.sort(function(a, b) {
+					if (a.publisher < b.publisher) {
+						return 1;
+					} else if (a.publisher > b.publisher) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} else if (result == 3) {
+				//出版日が選択されている場合
+				//出版日の降順を表示
+				array.sort(function(a, b) {
+					if (a.publishDate < b.publishDate) {
+						return 1;
+					} else if (a.publishDate > b.publishDate) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
+				console.log(array);
+			} 
+		}
+		for (var j = 0; j < array.length; j++) {
+			$(".books").eq(j).html(array[j].source);
+		}
+	});
+});
